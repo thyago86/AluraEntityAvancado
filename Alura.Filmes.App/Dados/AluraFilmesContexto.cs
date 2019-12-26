d309eb5be6a89186468ec38fc0b33ba51db453ee
Thyago <thyago.bezerra@prolins.com.br>
2019-12-26 14:17:58 -0300

    first commit

---
+++ Alura.Filmes.App/Dados/AluraFilmesContexto.cs
@@ -0,0 +1,19 @@
+﻿using Alura.Filmes.App.Negocio;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alura.Filmes.App.Dados {
+    public class AluraFilmesContexto : Microsoft.EntityFrameworkCore.DbContext {
+
+        public Microsoft.EntityFrameworkCore.DbSet<Ator> Atores {get; set;}
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=MSSQLLocalDB;Database=AluraFilmes;Trusted_connection=true;");
+        }
+    }
+}
---
+++ Alura.Filmes.App/Program.cs
@@ -0,0 +1,18 @@
+﻿using Alura.Filmes.App.Dados;
+
+namespace Alura.Filmes.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //select * from actor 
+            using (var contexto = new AluraFilmesContexto()) {
+                foreach(var ator in contexto.Atores) {
+                    System.Console.WriteLine(ator);
+                }
+            }
+            
+        }
+    }
+}
